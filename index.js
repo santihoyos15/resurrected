72d6a204716d552e0a27012880efbd3c3e2bc366
santihoyos15 <santiagohoyos2808@gmail.com>
2021-01-16 18:10:52 -0500

    Canvas figure added

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-var imagen = document.getElementById("macy");
-imagen.addEventListener("click" , xd);
+// Canvas definition
+var d = document.getElementById("eiffel");
+var canvas = d.getContext("2d");
+var colorStyle = "#521751";
+var canvasHeight = d.height;
+var canvasWidth = d.width;
 
-function xd(oe){
-    console.log(oe);
-    console.log(oe.offsetX , oe.offsetY);
+setInterval(drawEiffel, 2000);
+setInterval(() => {
+    canvas.clearRect(2, 2, canvasWidth -4, canvasHeight- 4);
+}, 4000);
+
+
+//Borders
+drawLine(colorStyle , 1 , 1, 1, canvasWidth -1);
+drawLine(colorStyle , 1 , canvasWidth -1, canvasWidth - 1, canvasWidth - 1);
+drawLine(colorStyle , 1 , 1, canvasWidth - 1 , 1);
+drawLine(colorStyle , canvasWidth - 1 , 1 , canvasWidth - 1, canvasWidth - 1);
+
+// Functions
+
+function drawEiffel(){
+    var aleatoryNumber = aleatory(1,30);
+    var lines = parseInt(aleatoryNumber);
+    var l = 0;
+    var iy, fx;
+
+    for (l = 0; l < lines; l++)
+    {
+        iy = canvasWidth / lines * l;
+        fx = canvasWidth / lines * (l + 1);
+        drawLine(colorStyle , 1 , iy, fx, 299);
+    }
+
+
 }
+
+function drawLine(color, initialX, initalY, finalX, finalY){
+    canvas.beginPath();          // Path opening.
+    canvas.strokeStyle = color;  // Stroke style definition.
+    canvas.moveTo(initialX , initalY);    // Stroke start.
+    canvas.lineTo(finalX , finalY);    // Stroke finished.
+    canvas.stroke();             // Stroke application.
+    canvas.closePath();           // Path closing.
+}
+
+function aleatory(min, max){ //Max Min Random number generator
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result;
+}
+
